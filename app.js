23f17c6ed09df1eb50d02b6311aa8d625117f276
Eduar Ordonez <eduarord@unicauca.edu.co>
2022-10-28 22:43:21 -0500

    update commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 var studentsRouter = require('./routes/students');
 var usersRouter = require('./routes/users');
@@ -10,13 +11,16 @@ const authRouter = require('./routes/auth');
 
 var app = express();
 app.set('view engine', 'pug')
+const DIRECTORIO_PERMITIDO_CORS = ["http://localhost:5500", "https://cariai.com"];
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors({
+  origin: DIRECTORIO_PERMITIDO_CORS
+}));
 app.use('/students', studentsRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
@@ -38,5 +42,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
