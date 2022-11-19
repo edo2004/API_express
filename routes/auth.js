cd9cf3c1034379bd8877b27dae267926e596ebea
Eduar Ordonez <eduarord@unicauca.edu.co>
2022-11-19 11:36:24 -0500

    modificacion status error

--- routes/auth.js
+++ routes/auth.js
@@ -31,15 +31,20 @@ router.post('/', upload.none(), function(req, res, next) {
   const pass = req.body?.password;
   const student = students.find(e => e.email == email);
   if (!student){
-    res.status(404);
-    res.json({error: 'El email no corresponde a un estudiante'});
+    res.status(200);
+    res.json({error: 'El email no corresponde a un estudiante',
+              codeError:2});
   }
   else {
-    if(student.password == pass)
+    if(student.password == pass){
+      res.status(200);
+      student.codeError = "1"
       res.json(student);
+  }
     else{
-      res.status(400);
-      res.json({error: 'Contraseña incorrecta'});
+      res.status(200);
+      res.json({error: 'Contraseña incorrecta',
+      codeError:2});
     }
   }
 });
