632b6b5924cd04643fb9d69e5023cbf47fd20ec3
Eduar Ordonez <eduarord@unicauca.edu.co>
2022-11-27 13:03:13 -0500

    modificar datos estudiantes

--- data/students.js
+++ data/students.js
@@ -1,22 +1,42 @@
 module.exports = [
   {
-    name: 'Eduar',
+    name: 'Andres',
     semester: 1,
-    code: 100614123123,
+    code: 100616930261,
+    email: 'andres@unicauca.edu.co',
+    celular: '310112233',
+    carrer: 'Tecnología en Telemática',
+    password: 'andres123',
+    enrolled:true
+  },
+  {
+    name: 'Maria',
+    semester: 3,
+    code: 100616020026,
+    email: 'maria@unicauca.edu.co',
+    celular: '310112233',
+    carrer: 'Ingeniería de Sistemas',
+    password: 'maria123',
+    enrolled: false
+  },
+  {
+    name: 'Victor',
+    semester: 7,
+    code: 100616020777,
     email: 'eduar@unicauca.edu.co',
     celular: '310112233',
     carrer: 'Ingeniería electrnoica y telecomunicaciones',
-    password: '123',
+    password: 'victor123',
     enrolled:true
   },
   {
     name: 'Sebastian',
-    semester: 3,
-    code: 100614123123,
+    semester: 1,
+    code: 100612036183,
     email: 'sebastian@unicauca.edu.co',
     celular: '310112233',
-    carrer: 'Ingeniería electrnoica y telecomunicaciones',
-    password: '124',
+    carrer: 'Ingeniería en Automática Industrial',
+    password: 'sebastian123',
     enrolled: false
   }
 ]
